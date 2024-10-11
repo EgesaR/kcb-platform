81ea8430f38194d6f795fc36bd4264485c6208b1
egesaraymond644 <egesaraymond644@gmail.com>
2024-10-11 19:39:03 +0000

    Night's Oct 11 22:38 commit

--- src/components/Portifolio/Hero.tsx
+++ src/components/Portifolio/Hero.tsx
@@ -2,7 +2,7 @@ import HeroContent from "./HeroContent"
 
 const Hero = ({children}) => {
   return(
-    <div className="w-full h-[50vh] flex flex-col relative justify-center items-center mt-28 sm:mt-20 sm:h-[85vh]">
+    <div className="w-full h-[80vh] sm:h-[50vh] flex flex-col relative justify-center items-center mt-28 sm:mt-20 sm:h-[85vh]">
       <HeroContent>
         {children}
       </HeroContent>
--- src/components/Portifolio/HeroContent.tsx
+++ src/components/Portifolio/HeroContent.tsx
@@ -7,7 +7,7 @@ const HeroContent = ({children}) => {
         <motion.div
             initial="hidden"
             animate="visible"
-            className="flex flex-row items-center justify-center px-20 mt-40 w-full h-full z-[20]">
+            className="flex flex-row items-center justify-center px-5 sm:px-20 mt-40 w-full h-full z-[20]">
             <div className="h-full w-full flex flex-row gap-5 justify-center m-auto text-start">
                 {children}
             </div>
